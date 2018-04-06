70ac9fea7774b5d467013855168d3c36b56b9861
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-04-06 10:56:31 -0500

    modify run_copy_and_replace_config_files.py

--- scripts/run_copy_and_replace_config_files.py
+++ scripts/run_copy_and_replace_config_files.py
@@ -8,7 +8,7 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--in_dir',               type=str, default="/home/tnguye46/allostery/restraint/equilibrate/adk")
+parser.add_argument('--in_dir',               type=str, default="/home/tnguye46/allostery/restraint/production/adk_amp")
 parser.add_argument('--window_range',         type=str, default="54 177")
 parser.add_argument('--config_file',          type=str, default="configuration.namd")
 parser.add_argument('--launch_file',          type=str, default="/home/tnguye46/allostery/launch_cpu.sh")
@@ -41,10 +41,9 @@ def _replace(infile, outfile, keyworks):
             handle.write(outline)
     return None
 
-KEYWORDS1 = {"adk":"adk_amp", "receptor.prmtop":"complex.prmtop", "receptor.pdb":"complex.pdb"}
-KEYWORDS2 = {"colvar_files":"colvar_files_adk_amp"}
+KEYWORDS1 = {"adk_amp":"adk_atp", "receptor.prmtop":"complex.prmtop", "receptor.pdb":"complex.pdb"}
+KEYWORDS2 = {"colvar_files_adk_amp":"colvar_files_adk_atp"}
 
-begin = int()
 begin, end = [int(s) for s in args.window_range.split()]
 print("window_range", begin, end)
 
